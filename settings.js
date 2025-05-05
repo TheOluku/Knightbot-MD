4e223c7c00792cbc6235bd84248cf5d5ae76f86e
TheOluku <danieloluku9@gmail.com>
2025-05-05 12:03:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Danluk Personal Assistant",
+  botOwner: 'Danluk', // Your name
+  ownerNumber: '2349074860522', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
